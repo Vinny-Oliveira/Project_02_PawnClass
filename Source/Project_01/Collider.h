331d2a58d9336f64cc4d665f70c7cbf98422ae19
Vinicius Oliveira <45839644+Vinny-Oliveira@users.noreply.github.com>
2020-07-18 22:09:26 -0400

    Added Adversaries package

---
+++ Source/Project_01/Collider.h
@@ -0,0 +1,40 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Pawn.h"
+#include "Collider.generated.h"
+
+UCLASS()
+class PROJECT_01_API ACollider : public APawn
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this pawn's properties
+	ACollider();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	// Called to bind functionality to input
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+
+	UPROPERTY(VisibleAnywhere, Category = "Mesh")
+	UStaticMeshComponent* MeshComponent{ nullptr };
+
+	FORCEINLINE UStaticMeshComponent* GetMeshComponent() { return MeshComponent; }
+	FORCEINLINE SetMeshComponent(UStaticMeshComponent* Mesh) { MeshComponent = Mesh; }
+
+	UPROPERTY(VisibleAnywhere, Category = "Mesh")
+	USphereComponent* SphereComponent{ nullptr };
+
+	FORCEINLINE USphereComponent* GetSphereComponent() { return SphereComponent; }
+	FORCEINLINE SetSphereComponent(USphereComponent* Sphere) { SphereComponent = Sphere; }
+};
