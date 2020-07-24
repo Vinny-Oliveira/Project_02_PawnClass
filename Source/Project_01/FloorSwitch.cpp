8d9ac67576c0ad3207355eab58aff734dae7d298
Vinicius Oliveira <45839644+Vinny-Oliveira@users.noreply.github.com>
2020-07-24 18:30:01 -0400

    Started binding the trigger box

--- Source/Project_01/FloorSwitch.cpp
+++ Source/Project_01/FloorSwitch.cpp
@@ -10,6 +10,17 @@ AFloorSwitch::AFloorSwitch()
  	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
 
+	TriggerBox = CreateDefaultSubobject<UBoxComponent>(TEXT("TriggerBox"));
+	RootComponent = TriggerBox;
+
+	TriggerBox->OnComponentBeginOverlap.AddDynamic(this, &AFloorSwitch::OnOverlapBegin);
+	TriggerBox->OnComponentEndOverlap.AddDynamic(this, &AFloorSwitch::OnOverlapEnd);
+
+	FloorSwitch = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("FloorSwitch"));
+	FloorSwitch->SetupAttachment(GetRootComponent());
+
+	Door = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("Door"));
+	Door->SetupAttachment(GetRootComponent());
 }
 
 // Called when the game starts or when spawned
@@ -26,3 +37,10 @@ void AFloorSwitch::Tick(float DeltaTime)
 
 }
 
+void AFloorSwitch::OnOverlapBegin(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult) {
+	UE_LOG(LogTemp, Warning, TEXT("OVERLAP BEGIN"));
+}
+
+void AFloorSwitch::OnOverlapEnd(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex) {
+	UE_LOG(LogTemp, Warning, TEXT("OVERLAP END"));
+}
