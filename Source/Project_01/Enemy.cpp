ab8f9a2d2022ed965b4c41aff037b06b04a9c313
Vinicius Oliveira <vr.sena@gmail.com>
2020-08-05 18:52:19 -0400

    Enemy chases after the player after the attack animation ends

--- Source/Project_01/Enemy.cpp
+++ Source/Project_01/Enemy.cpp
@@ -92,6 +92,7 @@ void AEnemy::CombatSphereOnOverlapEnd(UPrimitiveComponent* OverlappedComponent, 
 }
 
 void AEnemy::MoveToTarget(AMain* Target) {
+	UE_LOG(LogTemp, Warning, TEXT("MoveToTarget Called"));
 	SetEnemyMovementStatus(EEnemyMovementStatus::EEMS_MoveToTarget);
 
 	if (AIController) {
